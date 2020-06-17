32a3698a07937b68eee54f07dd0195514acb060b
Zuhaib Ahmed <zuhaibzulfiqarahmed@gmail.com>
2020-06-17 16:18:59 -0400

    Allows user to search for genes with entrez, ensembl, or hgnc IDs

--- theApp/app.R
+++ theApp/app.R
@@ -17,6 +17,11 @@ datasets <- lapply(fls, function(x) {
 names(datasets) <- fls
 names(datasets) <- str_replace_all(names(datasets), "\\.txt", "")
 names(datasets) <- str_replace_all(names(datasets), "\\..+long_", "")
+# Need to remove the decimal part of the GSE148729 dataset gene IDs, to make it easier to search
+datasets$GSE148729_Calu3_mockInfection$Gene <-  str_replace_all(datasets$GSE148729_Calu3_mockInfection$Gene, "\\..*", "")
+datasets$GSE148729_Calu3_sarsCov1$Gene <- str_replace_all(datasets$GSE148729_Calu3_sarsCov1$Gene, "\\..*", "")
+datasets$GSE148729_Calu3_sarsCov2$Gene <- str_replace_all(datasets$GSE148729_Calu3_sarsCov2$Gene, "\\..*", "")
+
 
 # Reads in the normalized expression data
 fls2 <- unlist(lapply(list.files()[grep("data_", list.files())], function(d) {
@@ -31,6 +36,14 @@ names(normData) <- fls2
 names(normData) <- str_replace_all(names(normData), "\\.txt", "")
 names(normData) <- str_replace_all(names(normData), "\\..+normalized_", "")
 
+fls3 <- paste0("./geneMappings/", list.files("./geneMappings")[grep("mappings_", list.files("./geneMappings"))])
+mappings <- lapply(fls3, function(x) {
+  read.table(x, header = T, sep = "\t")
+})
+names(mappings) <- fls3
+names(mappings) <- str_replace_all(names(mappings), "\\.txt", "")
+names(mappings) <- str_replace_all(names(mappings), "\\..+mappings_", "")
+
 # Reads in file specifying which samples correspond to which columns in the normalized data.
 # This is the help disply the desired data in tabular format
 groupToColumns <- read.table("groupToColumns.txt", header = T, sep = "\t")
@@ -38,6 +51,12 @@ groupToColumns$File <- str_replace_all(groupToColumns$File, "\\.txt", "")
 groupToColumns$File <- str_replace_all(groupToColumns$File, ".+normalized_", "")
 
 
+# adf <- datasets$GSE148729_Calu3_sarsCov2
+# bdf <- mappings$ensembl
+# testEntrez <- 6013
+# g <- bdf[which(bdf$ENTREZ_ID == testEntrez),1]
+# adf[grep(g[1], adf[,1]),]
+# adf[grep(g[2], adf[,1]),]
 
 # Define UI for miles per gallon app ----
 ui <- pageWithSidebar(
@@ -48,6 +67,8 @@ ui <- pageWithSidebar(
   # Sidebar panel for inputs ----
   sidebarPanel(
     checkboxGroupInput("selDatasets", "Select Datasets", choices = names(datasets)),
+    radioButtons("selID", label = "What type of gene ID?", 
+                 choices = list("Gene Symbol" = "GENE_SYMBOL", "Ensembl ID" = "ENSEMBL_GENE_ID", "Entrez ID" = "ENTREZ_ID")),
     textAreaInput("selGenes", "Genes of Interest", height = "200px"),
     radioButtons("collapseLines", label = "Collapse lines?",
                  choices = list("Yes" = "yes", "No" = "no"), 
@@ -74,7 +95,7 @@ server <- function(input, output) {
     minTimePoint <- timePoints[1,2]
     # If the lines will collapse, then raise each y value to the power of to so lines don't clump together
     if (collapseLinesFlag == T) {
-      yVals <- 2^c(0, cumsum(timePoints$log2FoldChange))
+      yVals <- c(1, cumprod(2^(timePoints$log2FoldChange)))
     } else {
       yVals <- c(0, cumsum(timePoints$log2FoldChange))
     }
@@ -99,16 +120,32 @@ server <- function(input, output) {
     return(lapply(input$selDatasets, function(y) {
       ds <- datasets[[y]]
       genesOfInterest <- strsplit(input$selGenes, split = '[\r\n]')[[1]] 
+      selectedIDType <- input$selID
+      dataset_primaryID <- groupToColumns$PrimaryID[which(groupToColumns$Group == y)]
       lst <- lapply(genesOfInterest, function(x) {
-        ret <- ds[grep(x, ds[,1]),]
-        if (nrow(ret) == 0) {
+        # First, map the users query genes to the gene IDs present in their dataset of interest
+        m <- mappings[[dataset_primaryID]]
+        g <- unique(m[grep(paste0("^", x, "$"), m[[selectedIDType]]),1])
+        retLst <- lapply(g, function(z) {
+          ret <- ds[grep(paste0("^", z, "$"), ds[,1]),]
+          if (nrow(ret) == 0) {
+            return(NA)
+          } else {
+            ret[,1] <- x
+            return(ret)
+          }
+        })
+        retLst <- retLst[which(!(is.na(retLst)))]
+        #print(retLst)
+        if (length(retLst) == 0) {
           return(NA)
         } else {
-          return(ret)
+          return(retLst)
         }
       })
       genesNotFound <- genesOfInterest[which(is.na(lst))]
       lst <- lst[which(!is.na(lst))]
+      lst <- unlist(lst, recursive = F)
       lns <- lapply(lst, function(x) {
         if (input$collapseLines == "yes") {
           return(makeLine(x, T))
@@ -186,3 +223,12 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
+
+
+
+
+
+
+
+
+
